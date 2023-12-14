4716ba0a801a3420787cfc434ec1a8117913b740
alexbgh1 <alex.garnica@alumnos.uach.cl>
2023-12-13 22:03:15 -0300

    Pruebas Hero quitando bg-opacity

--- src/components/BenefitsCardItem.tsx
+++ src/components/BenefitsCardItem.tsx
@@ -10,7 +10,7 @@ const BenefitsCardItem = ({ benefit }: { benefit: Benefit }) => {
   const { name, details, testing, limits } = benefit;
 
   return (
-    <article className="before:w-full  min-w-[240px] max-w-[350px] before:h-full before:absolute before:z-[-1] before:opacity-50 before:transition-all before:duration-300 before:transform before:hover:transform before:hover:skew-y-[1deg] before:hover:scale-105 before:hover:shadow-md before:hover:border-gray-400 before:bg-third/40 before:border before:border-gray-300 before:rounded-md before:shadow-sm bg-opacity-50 transition-all duration-300 transform hover:transform hover:skew-y-[1deg] hover:scale-105 hover:shadow-md hover:border-gray-400 bg-third/40 border border-gray-300 rounded-md shadow-sm">
+    <article className="before:w-full  min-w-[240px] max-w-[350px] before:h-full before:absolute before:z-[-1] before:opacity-50 before:transition-all before:duration-300 before:transform before:hover:transform before:hover:skew-y-[1deg] before:hover:scale-105 before:hover:shadow-md before:hover:border-gray-400 before:bg-third/40 before:border before:border-gray-300 before:rounded-md before:shadow-sm bg-opacity-50 transition-all duration-300 transform hover:transform hover:skew-y-[1deg] hover:scale-105 hover:shadow-md hover:border-gray-400 bg-third border border-gray-300 rounded-md shadow-sm">
       {/* Header */}
       <BenefitsCardItemHeader name={name} details={details} />
       {/* Body */}
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -6,7 +6,7 @@ const Body = () => {
   return (
     <>
       <Hero />
-      <main className="flex-1 justify-start pb-8 border-t-[1px] border-accent bg-third/25 w-full min-w-full container flex flex-col items-center">
+      <main className="flex-1 justify-start pb-8 border-t-[1px] border-accent bg-primary w-full min-w-full container flex flex-col items-center">
         <h2 className="mt-8 text-3xl sm:text-4xl font-bold text-center text-primary">Empresas asociadas</h2>
         <Benefits />
       </main>
